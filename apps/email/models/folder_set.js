c65c5a0302b11127c2130e570439d8d019246f1e
Eric Kidd <git@randomhacks.net>
2010-03-09 12:52:09 -0500

    apps/email: Autogenerate FolderSet class
    This will represent top-level entries in our SourceView sidebar.

---
+++ apps/email/models/folder_set.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Email.FolderSet
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Email */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Email.FolderSet = SC.Record.extend(
+/** @scope Email.FolderSet.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/email/tests/models/folder_set.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Email.FolderSet Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Email module test ok equals same stop start */
+
+module("Email.FolderSet");
+
+// TODO: Replace with real unit test for FolderSet
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
